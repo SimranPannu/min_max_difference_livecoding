d027cefe17d3868e0a6aab54ee851f2e49a0aca1
Simran Pannu <simpannu14@gmail.com>
2019-10-29 19:20:42 -0400

    not complete

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    the_array =[15,22,84,14,88,23]
+    #the_highest_value =88
+    #the_smallest_number =14
+    #the_difference =74
+
+    the_highest_value =""
+    the_smallest_number=""
+    the_difference=""
+    i=0
+    a
+    for i in the_array:
+        if (the_array[i]<the_array[i+1]):
+            the_highest_value=the_array[i+1]
+        else:
+            the_highest_value=the_array[i]
+        i++
+        print("Highest Value:"+the_highest_value)
+
+    if __name__ == "__main__":
+    main()
